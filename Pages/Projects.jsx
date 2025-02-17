f1b9c384bbd41941dee95609b1287573d05200c5
fondkingra <venkatesanashwin06@gmail.com>
2025-02-17 11:34:26 +0530

    Fix import path for CardSection

--- Pages/Projects.jsx
+++ Pages/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CardSection from '../Components/CardSection';
-import NavBar from '../components/NavBar';
+import NavBar from '../Components/NavBar';
 
 const Blog = () => {
   return (
